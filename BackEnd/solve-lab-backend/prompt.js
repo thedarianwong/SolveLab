d96b7688cb008833f01dac2d779500ddcb4e0d3c
thedarianwong <thedarianwong@gmail.com>
2023-05-20 18:48:28 -0700

    Modified Prompts and Steps to have some Outputs

---
+++ BackEnd/solve-lab-backend/prompt.js
@@ -0,0 +1,16 @@
+// const prompt =
+//   "You are a math tutor. A student comes to you with a math problem and provides the correct solution. Your task is to explain the step-by-step process to solve the problem, making it clear and understandable to the student. Assume the student has a basic understanding of the topic." +
+//   "Problem: [Insert the math problem here]" +
+//   "Correct Solution: [Insert the correct solution here]" +
+//   "Please provide a step-by-step explanation of how to arrive at the solution. Start with the given problem and break down each step with clear explanations." +
+//   "Step 1: Start with the given problem." +
+//   "Explanation: The first step is to carefully read and understand the problem. Make sure to identify any given information and the desired solution. This will provide a clear understanding of what needs to be solved." +
+//   "Step 2: Identify the relevant concepts and formulas." +
+//   "Explanation: Based on the problem, identify the mathematical concepts and formulas that are applicable. This will help you choose the appropriate approach to solve the problem." +
+//   "Step 3: [Continue with the step-by-step explanation, providing clear explanations of each step.]" +
+//   "Continue with the remaining steps until the solution is reached. Be sure to explain each step in a clear and understandable manner, using appropriate mathematical language and notation.";
+
+const prompt =
+  "I have a math problem and I know the correct solution. The problem is: '${problem}'. The solution is: '${solution}'. However, I'm not sure how to get to that solution. Could you provide a step-by-step guide explaining how to solve the problem and arrive at that solution?";
+
+module.exports = prompt;
--- BackEnd/solve-lab-backend/server.js
+++ BackEnd/solve-lab-backend/server.js
@@ -1,23 +1,26 @@
+const promptTemplate = require("./prompt");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // to parse JSON request bodies
 
-app.get("/", (req, res) => {
-  res.send("Math Solver Server is Running!");
-});
-
 app.post("/solve", async (req, res) => {
   const problem = req.body.problem;
 
   try {
-    const solution = await getSolutionFromWolfram(problem);
+    const solution = "x = 2, y = 3/2";
     const steps = await getStepsFromOpenAI(problem, solution);
-
     res.json({ solution, steps });
   } catch (err) {
     res.status(500).json({ error: "This is too Difficult but I will Try!" });
@@ -26,36 +29,56 @@ app.post("/solve", async (req, res) => {
 });
 
 async function getSolutionFromWolfram(problem) {
-  const response = await axios.get("http://api.wolframalpha.com/v2/query", {
-    params: {
-      input: problem,
-      output: "JSON",
-      appid: process.env.WOLFRAM_APP_ID,
-    },
-  });
+  try {
+    const response = await axios.get("http://api.wolframalpha.com/v2/query", {
+      params: {
+        input: problem,
+        output: "JSON",
+        appid: process.env.WOLFRAM_APP_ID,
+      },
+    });
 
-  // Parse response to get solution...
+    const pods = response.data.queryresult?.pods;
+    if (!pods || pods.length < 2) {
+      throw new Error("Invalid response data");
+    }
 
-  return solution;
+    const solutionPod = pods[1];
+
+    if (
+      !solutionPod ||
+      !solutionPod.subpods ||
+      solutionPod.subpods.length === 0
+    ) {
+      throw new Error("No solution found");
+    }
+
+    const solution = solutionPod.subpods[0].plaintext;
+
+    return solution;
+  } catch (err) {
+    console.error(err.response ? err.response.data : err);
+    throw err; // re-throw the error to propagate it to the calling function
+  }
 }
 
 async function getStepsFromOpenAI(problem, solution) {
-  const response = await axios.post(
-    "https://api.openai.com/v1/engines/davinci-codex/completions",
-    {
-      prompt: `Problem: ${problem}\nSolution: ${solution}\nExplain the steps to solve this problem.`,
-      max_tokens: 200,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-    }
-  );
+  const prompt = promptTemplate
+    .replace("${problem}", problem)
+    .replace("${solution}", solution);
 
-  // Parse response to get steps...
-
-  return steps;
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      max_tokens: 2000,
+      temperature: 0, // Adjust the temperature as needed for desired output randomness
+    });
+    const steps = response.data.choices[0].text.trim();
+    return steps;
+  } catch (err) {
+    console.error(err.response ? err.response.data : err); // Log the error
+  }
 }
 
 const port = process.env.PORT || 5000;
